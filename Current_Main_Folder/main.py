130abf20b75c7aa9230ead28a792b114f88b0ea3
Garry Keogh <garrypkeogh@gmail.com>
2019-01-29 00:31:16 +0000

    Debugged WiFi and MQTT login Tested main function

--- Current_Main_Folder/main.py
+++ Current_Main_Folder/main.py
@@ -1,3 +1,5 @@
+
+
 import os
 import machine
 from machine import Pin
@@ -5,6 +7,7 @@ import time
 import credentials
 import connect
 from umqtt.simple import MQTTClient
+import ubinascii
 
 
 # some variables which may be called as Globals
@@ -15,22 +18,33 @@ opto_2 = False  # flag set by interrupt on rising or falling value
 door_up = False  # main function must refresh these values "while True"
 door_down = False  # main function must refresh these values "while True"
 cmd_down = False  # a flag used to see if a "dead-man" MQTT signal is present to lower the door
+incoming_msg = "" # empty string until we use it later...
+blank_incoming_msg = "" # empty string until we use it later...
+
+
 
 # set up some OUTPUT pins
 ONBOARD_LED = machine.Pin(22, machine.Pin.OUT)
+
 ONBOARD_LED.value(1)
+
 RLY_UP = machine.Pin(19, machine.Pin.OUT)
+
 RLY_UP.value(0)
+
 RLY_STOP = machine.Pin(23, machine.Pin.OUT)
+
 RLY_STOP.value(0)
+
 RLY_DOWN = machine.Pin(18, machine.Pin.OUT)
 
 RLY_DOWN.value(0)
+
 AUDIO_1 = machine.Pin(5, machine.Pin.OUT)
+
 AUDIO_2 = machine.Pin(17, machine.Pin.OUT)
 
 # handle interrupt requests from input Pins
-
 
 def input_callback(p):
     global opto_1
@@ -54,29 +68,33 @@ def input_callback(p):
         elif not OPT_2.value():
             opto_2 = False
 
-
 # set up some INPUT pins and IRQ triggers to catch inputs
 FOB_A = machine.Pin(34, machine.Pin.IN)
+
 FOB_A.irq(trigger=Pin.IRQ_RISING, handler=input_callback)
 
 FOB_B = machine.Pin(35, machine.Pin.IN)
+
 FOB_B.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=input_callback)
 
 FOB_C = machine.Pin(32, machine.Pin.IN)
+
 FOB_A.irq(trigger=Pin.IRQ_RISING, handler=input_callback)
 
 FOB_D = machine.Pin(33, machine.Pin.IN)
+
 FOB_D.irq(trigger=Pin.IRQ_RISING, handler=input_callback)
 
 OPT_1 = machine.Pin(25, machine.Pin.IN)
+
 OPT_1.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=input_callback)
 
 OPT_2 = machine.Pin(26, machine.Pin.IN)
+
 OPT_2.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=input_callback)
 
+
 # what to do if Button A is pressed on fob
-
-
 def button_A():
     global lifting
     global lowering
@@ -89,6 +107,7 @@ def button_A():
             lift_now()
 
 
+# how to stop the door once moving
 def stop_now():
     entry = time.ticks_ms()
     RLY_STOP.value(1)
@@ -97,6 +116,7 @@ def stop_now():
             RLY_STOP.value(0)
 
 
+# move the door upwards
 def lift_now():
     entry = time.ticks_ms()
     RLY_UP.value(1)
@@ -122,70 +142,85 @@ def button_B():
         lowering = False
         RLY_DOWN.value(0)
 
-
 # run WiFi connect script
 connect.do_connect()
 
 #  MQTT server and creds to connect
 SERVER = "m20.cloudmqtt.com"
-USER = "XXXXXX"
-PASSWORD = "XXXXXX"
-PORT = "XXXXX"
+USER = "rmfhlxgf"
+PASSWORD = "4e8hGCapvTQv"
+PORT = "14928"
 CLIENT_ID = ubinascii.hexlify(machine.unique_id())
 
 
-def sub_cb(topic, msg):  # the callback to handle MQTT messages
+
+# the callback to handle MQTT messages
+def sub_cb(topic, msg):
     print((topic, msg))
     global cmd_up
+    global incoming_msg
     if topic == b"otto/cmd":
-        if msg == b"up"
-        cmd_up = True
-
-#  Most of the program is here
-
-
-def main():
-    client = MQTTClient(CLIENT_ID)
-    client.connect(host=SERVER, user=USER, password=PASSWORD, port=PORT)
-    print("Connected to MQTT broker ", SERVER)
+        if msg == b"up":
+          cmd_up = True
+          incoming_msg = msg
+ 
+ 
+  #  Most of the program is here
+def main(server=SERVER, port=PORT, user=USER, password=PASSWORD):
+    client = MQTTClient(CLIENT_ID, server, port, user, password)
     client.set_callback(sub_cb)
+    client.connect()
+    print("Connected to MQTT broker ", SERVER)
     client.subscribe("otto/#")
     global lifting
     global door_down
+    global incoming_msg
+    global blank_incoming_msg
     last_door_down = 1
     last_lifting = 0
     try:
-        while True:
-            if opto_2 and not opto_1:
-                door_up = True
-            else:
-                door_up = False
-            if opto_2 and opto_1:
-                door_down = True
-            else:
-                door_down = False
-            if lifting != last_lifting:
-                if lifting:
-                entry = time.ticks_ms()
-                   if time.ticks_ms() >= entry + 10000:
-                        timeout = True
-                    if door_up | timeout:
-                        lifting = False
-                        timeout = False
-                        stop_now()
+      while True:
+        if opto_1 and not opto_2:
+          door_up = True
+        else:
+          door_up = False
+        if not opto_1 and not opto_2:
+          door_down = True
+        else:
+            door_down = False
+        if lifting != last_lifting:
+          if lifting:
+            entry = time.ticks_ms()
+            if time.ticks_ms() >= entry + 10000:
+              timeout = True
+              if door_up | timeout:
+                lifting = False
+                timeout = False
+                stop_now()
             if door_down != last_door_down:
-                if door_down:
-                    stop_now()
-            last_lifting = lifting
-            last_door_down = door_down
-            client.check_msg()
+              if door_down:
+                stop_now()
+        if incoming_msg != blank_incoming_msg:
+          client.publish(b"otto/stat", b"Got a new message")
+        last_lifting = lifting
+        last_door_down = door_down
+        incoming_msg = ""
+        blank_incoming_msg = ""
+        client.check_msg()
+
+
     except KeyboardInterrupt:
-
-        print("Graceful exit by keyboard interrupt")
-
+      print("Graceful exit by keyboard interrupt")
+      client.publish(b"otto/stat", b"Graceful exit by keyboard interrupt")
     finally:
-        print("Code stopped, idle now")
+      print("Code crashed, idle now")
+      client.publish(b"otto/stat", b"Code stopped, ESP32 idle now")
 
-
+#  run the main module
 if __name__ == '__main__':
     main()
+
+
+
+
+
--- connect.py
+++ connect.py
@@ -7,9 +7,9 @@ def do_connect():
     if not sta_if.isconnected():
         print('connecting to network now...')
         sta_if.active(True)
-        sta_if.ifconfig((credentials.static, credentials.mask,
-                         credentials.gateway, credentials.DNS))
+        sta_if.ifconfig((credentials.static, credentials.mask, credentials.gateway, credentials.DNS))
         sta_if.connect(credentials.SSID, credentials.password)
         while not sta_if.isconnected():
             pass
     print('network config:', sta_if.ifconfig())
+
