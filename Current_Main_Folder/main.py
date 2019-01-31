6c6193f6d2c79f399585a45e466fd810db930076
Garry Keogh <garrypkeogh@gmail.com>
2019-01-31 00:42:53 +0000

    Debugged functions - no opto switches

--- Current_Main_Folder/main.py
+++ Current_Main_Folder/main.py
@@ -1,5 +1,7 @@
 
 
+
+
 import os
 import machine
 from machine import Pin
@@ -20,76 +22,89 @@ door_down = False  # main function must refresh these values "while True"
 cmd_down = False  # a flag used to see if a "dead-man" MQTT signal is present to lower the door
 incoming_msg = "" # empty string until we use it later...
 blank_incoming_msg = "" # empty string until we use it later...
+timeout = False # a timer used to stop the door if opto fails
 
 
 
 # set up some OUTPUT pins
-ONBOARD_LED = machine.Pin(22, machine.Pin.OUT)
+#ONBOARD_LED = machine.Pin(22, machine.Pin.OUT)
 
-ONBOARD_LED.value(1)
+#ONBOARD_LED.value(1)
 
-RLY_UP = machine.Pin(19, machine.Pin.OUT)
+RLY_UP = machine.Pin(22, machine.Pin.OUT)
 
-RLY_UP.value(0)
+RLY_UP.value(1)
 
-RLY_STOP = machine.Pin(23, machine.Pin.OUT)
+RLY_STOP = machine.Pin(32, machine.Pin.OUT)
 
-RLY_STOP.value(0)
+RLY_STOP.value(1)
 
-RLY_DOWN = machine.Pin(18, machine.Pin.OUT)
+RLY_DOWN = machine.Pin(21, machine.Pin.OUT)
 
-RLY_DOWN.value(0)
+RLY_DOWN.value(1)
 
-AUDIO_1 = machine.Pin(5, machine.Pin.OUT)
+RLY_BEEPER = machine.Pin(33, machine.Pin.OUT)
 
-AUDIO_2 = machine.Pin(17, machine.Pin.OUT)
+RLY_BEEPER.value(1)
+
+
+
 
 # handle interrupt requests from input Pins
-
 def input_callback(p):
     global opto_1
     global opto_2
     if p == FOB_A:
+        print("input - Fob A")
         button_A()
+ 
     elif p == FOB_B:
+        print("input - Fob B")
         button_B()
+        
     elif p == FOB_C:
-        pass
+        print("input - Fob C")
+        button_C()
+        
     elif p == FOB_D:
         pass
     elif p == OPT_1:
-        if OPT_1.value():
+        if not OPT_1.value():
             opto_1 = True
-        elif not OPT_1.value():
+            print("opto_1 = True")
+        elif OPT_1.value():
             opto_1 = False
+            print("opto_1 = False")
     elif p == OPT_2:
-        if OPT_2.value():
+        if not OPT_2.value():
             opto_2 = True
-        elif not OPT_2.value():
+            print("opto_2 = True")
+        elif OPT_2.value():
             opto_2 = False
+            print("opto_2 = False")
 
 # set up some INPUT pins and IRQ triggers to catch inputs
-FOB_A = machine.Pin(34, machine.Pin.IN)
+FOB_A = machine.Pin(16, machine.Pin.IN)
 
 FOB_A.irq(trigger=Pin.IRQ_RISING, handler=input_callback)
 
-FOB_B = machine.Pin(35, machine.Pin.IN)
+FOB_B = machine.Pin(4, machine.Pin.IN)
 
 FOB_B.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=input_callback)
 
-FOB_C = machine.Pin(32, machine.Pin.IN)
+FOB_C = machine.Pin(0, machine.Pin.IN)
 
-FOB_A.irq(trigger=Pin.IRQ_RISING, handler=input_callback)
+FOB_C.irq(trigger=Pin.IRQ_RISING, handler=input_callback)
 
-FOB_D = machine.Pin(33, machine.Pin.IN)
+FOB_D = machine.Pin(2, machine.Pin.IN)
 
 FOB_D.irq(trigger=Pin.IRQ_RISING, handler=input_callback)
 
-OPT_1 = machine.Pin(25, machine.Pin.IN)
+OPT_1 = machine.Pin(12, machine.Pin.IN)
 
 OPT_1.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=input_callback)
 
-OPT_2 = machine.Pin(26, machine.Pin.IN)
+OPT_2 = machine.Pin(13, machine.Pin.IN)
 
 OPT_2.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=input_callback)
 
@@ -99,48 +114,67 @@ def button_A():
     global lifting
     global lowering
     if lifting:  # button_A stops lifting if already started
-        lifting = False
         stop_now()
-    if not door_up:  # DON't if we are fully open
-        if not lifting:  # okay to lift the door...
+        print("Stopping lifting now by button A")
+    elif not lifting:  # DON't if we are fully open
+            print("Door ready to lift, Button A, lifting now")
             lifting = True
             lift_now()
 
 
+
 # how to stop the door once moving
 def stop_now():
+    global lifting
+    global lowering
+    print("Stop command -stop_now function")
+    lifting = False
+    lowering = False
     entry = time.ticks_ms()
-    RLY_STOP.value(1)
-    while RLY_STOP.value():
+    RLY_STOP.value(0)
+    while not RLY_STOP.value():
         if time.ticks_ms() >= entry + 500:
-            RLY_STOP.value(0)
+            RLY_STOP.value(1)
 
 
 # move the door upwards
 def lift_now():
     entry = time.ticks_ms()
-    RLY_UP.value(1)
-    while RLY_UP.value():
+    RLY_UP.value(0)
+    while not RLY_UP.value():
         if time.ticks_ms() >= entry + 500:
-            RLY_UP.value(0)
+            RLY_UP.value(1)
 
 
 def button_B():
     global lifting
     global lowering
+    print("Lower command received")
     entry = time.ticks_ms()
-    while lifting:  # Stop first if the door is lifting
+    
+    if lifting:  # Stop first if the door is lifting
         lifting = False  # Stopped lifting
-        RLY_STOP.value(1)
+        print("Halting door to change direction")
+        RLY_STOP.value(0)
         while RLY_STOP.value():
             if time.ticks_ms() >= entry + 500:
-                RLY_STOP.value(0)
-    while FOB_B.value() | cmd_down:
+                RLY_STOP.value(1)
+
+    if FOB_B.value() | cmd_down:
         lowering = True
-        RLY_DOWN.value(1)
-    while not FOB_B.value() | cmd_down:
-        lowering = False
         RLY_DOWN.value(0)
+    elif not FOB_B.value():
+        lowering = False
+        RLY_DOWN.value(1)
+    elif not cmd_down:
+        lowering = False
+        RLY_DOWN.value(1)
+
+def button_C():
+  stop_now()
+  lifting = False
+  lowering = False
+  print("button C funciton called")
 
 # run WiFi connect script
 connect.do_connect()
@@ -159,10 +193,11 @@ def sub_cb(topic, msg):
     print((topic, msg))
     global cmd_up
     global incoming_msg
+    incoming_msg = str(msg)
     if topic == b"otto/cmd":
         if msg == b"up":
           cmd_up = True
-          incoming_msg = msg
+          button_A()
  
  
   #  Most of the program is here
@@ -171,7 +206,7 @@ def main(server=SERVER, port=PORT, user=USER, password=PASSWORD):
     client.set_callback(sub_cb)
     client.connect()
     print("Connected to MQTT broker ", SERVER)
-    client.subscribe("otto/#")
+    client.subscribe("otto/cmd")
     global lifting
     global door_down
     global incoming_msg
@@ -193,6 +228,7 @@ def main(server=SERVER, port=PORT, user=USER, password=PASSWORD):
             entry = time.ticks_ms()
             if time.ticks_ms() >= entry + 10000:
               timeout = True
+              print("timeout!")
               if door_up | timeout:
                 lifting = False
                 timeout = False
@@ -219,6 +255,9 @@ def main(server=SERVER, port=PORT, user=USER, password=PASSWORD):
 #  run the main module
 if __name__ == '__main__':
     main()
+
+
+
 
 
 
