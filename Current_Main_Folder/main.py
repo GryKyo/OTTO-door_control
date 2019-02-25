39347ba50a4314d20e147c651ca604859f0425bd
Garry Keogh <garrypkeogh@gmail.com>
2019-02-25 22:17:24 +0000

    Deleted clutter and print debug

--- Current_Main_Folder/main.py
+++ Current_Main_Folder/main.py
@@ -48,34 +48,25 @@ def input_callback(p):
     global opto_1
     global opto_2
     if p == FOB_A:
-        print("input - Fob A")
         button_A()
     elif p == FOB_B:
-        print("input - Fob B")
         button_B()
     elif p == FOB_C:
-        print("input - Fob C")
-        button_C()
+         button_C()
     elif p == FOB_D:
-        print("input - Fob D")
         button_D()
     elif p == RFID_DIO:
-        print("RFID reader DIO backup")
         button_A()
     elif p == OPT_1:
         if OPT_1.value():
             opto_1 = True
-            print("opto_1 = True")
-        elif not OPT_1.value():
+         elif not OPT_1.value():
             opto_1 = False
-            print("opto_1 = False")
     elif p == OPT_2:
         if OPT_2.value():
             opto_2 = True
-            print("opto_2 = True")
         elif not OPT_2.value():
             opto_2 = False
-            print("opto_2 = False")
     elif p == RFID_DIO.value():
         button_A()
 
@@ -102,9 +93,7 @@ def button_A():
     global lowering
     if lifting:  # button_A stops lifting if already started
         stop_now()
-        print("Stop lifting now - button A")
     elif not lifting and not door_up:  # DON't if we are fully open
-            print("Door ready to lift, Button A, lifting now")
             lifting = True
             lift_now()
 
@@ -112,11 +101,9 @@ def button_A():
 def button_B():
     global lifting
     global lowering
-    print("Lower command received")
     entry = time.ticks_ms()
     if lifting:  # Stop first if the door is lifting
         lifting = False  # Stopped lifting
-        print("Halting door to change direction")
         RLY_STOP.value(0)
         while not RLY_STOP.value():
             if time.ticks_ms() >= entry + 500:
@@ -133,7 +120,6 @@ def button_C():
   stop_now()
   lifting = False
   lowering = False
-  print("button C funciton called")
 
 #  what to do if fob BUTTON D is pressed - just sounding the beacon
 def button_D():
@@ -147,7 +133,6 @@ def button_D():
 def stop_now():
     global lifting
     global lowering
-    print("Stopping now - many reasons!")
     client.publish(b"otto/stat", b"stop")
     lifting = False
     lowering = False
@@ -262,9 +247,8 @@ try:
     last_door_up = door_up
     client.check_msg()
 except KeyboardInterrupt:
-  print("Graceful exit by keyboard interrupt")
-  client.publish(b"otto/stat", b"Graceful exit by keyboard interrupt")
+  client.publish(b"otto/stat", b" exit by keyboard interrupt")
 finally:
-  print("Code stopped, idle now")
   client.publish(b"otto/stat", b"Code stopped, ESP32 idle now")
+
 
